8f88eacdc399f1f578d2eca2ec4081b1f7b685ad
effepivi <franck.p.vidal@gmail.com>
2016-10-10 20:24:14 +0100

    Add Lab-02 about writing simple classes, overwriting operators, and using std::string instead of char*.

---
+++ Labs/Lab-02/src/Integer.cpp
@@ -0,0 +1,129 @@
+/**
+******************************************************************************
+*
+*	@file		Integer.cpp
+*
+*	@brief		Class used to manage an Integer number.
+*
+*	@version	1.0
+*
+*	@date		13/10/2015
+*
+*	@author		Your name
+*
+*
+******************************************************************************
+*/
+
+
+//******************************************************************************
+//	Include
+//******************************************************************************
+#include "Integer.h"
+
+
+//-----------------
+Integer::Integer()
+//-----------------
+//-----------------
+{
+    std::cout << "IN: Integer::Integer()" << std::endl;
+    
+    std::cout << "\t" << m_data << std::endl;
+    
+    m_data = 0;
+    
+    std::cout << "\t" << m_data << std::endl;
+    
+    std::cout << "OUT: Integer::Integer()" << std::endl;
+}
+
+
+//-----------------------------------------
+Integer::Integer(const Integer& anInteger):
+//-----------------------------------------
+        m_data(anInteger.m_data)
+//-----------------------------------------
+{
+    std::cout << "IN: Integer::Integer(const Integer& anInteger)" << std::endl;
+
+    std::cout << "\t" << m_data << std::endl;
+
+    std::cout << "OUT: Integer::Integer(const Integer& anInteger)" << std::endl;
+}
+
+
+//------------------------------
+Integer::Integer(int anInteger)
+//------------------------------
+//------------------------------
+{
+    std::cout << "IN: Integer::Integer(int anInteger)" << std::endl;
+
+    std::cout << "\t" << m_data << std::endl;
+
+    m_data = anInteger;
+
+    std::cout << "\t" << m_data << std::endl;
+
+    std::cout << "OUT: Integer::Integer(int anInteger)" << std::endl;
+}
+
+
+//-----------------
+Integer::~Integer()
+//-----------------
+{
+    std::cout << "IN: Integer::~Integer()" << std::endl;
+    std::cout << "OUT: Integer::~Integer()" << std::endl;
+}
+
+
+//---------------------------------------------------
+Integer& Integer::operator=(const Integer& anInteger)
+//---------------------------------------------------
+{
+    std::cout << "IN: Integer::operator=(const Integer& anInteger)" << std::endl;
+
+    std::cout << "\t" << m_data << std::endl;
+
+    m_data = anInteger.m_data;
+
+    std::cout << "OUT: Integer::operator=(const Integer& anInteger)" << std::endl;
+
+    return (*this);
+}
+
+
+//----------------------------------------
+Integer& Integer::operator=(int anInteger)
+//----------------------------------------
+{
+    std::cout << "IN: Integer::operator=(const Integer& anInteger)" << std::endl;
+
+    std::cout << "\t" << m_data << std::endl;
+
+    m_data = anInteger;
+
+    std::cout << "OUT: Integer::operator=(const Integer& anInteger)" << std::endl;
+
+    return (*this);
+}
+
+
+//-----------------------------------------------------------------------
+std::ostream& operator<<(std::ostream& aStream, const Integer& anInteger)
+//-----------------------------------------------------------------------
+{
+    aStream << anInteger.m_data;
+    return (aStream);
+}
+
+
+//-----------------------------------------------------------------
+std::istream& operator>>(std::istream& aStream, Integer& anInteger)
+//-----------------------------------------------------------------
+{
+    aStream >> anInteger.m_data;
+    return (aStream);
+}
