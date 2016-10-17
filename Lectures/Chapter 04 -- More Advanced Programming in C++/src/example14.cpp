92a569378bbe3b0e6502456ffb9bf7003c94426f
effepivi <franck.p.vidal@gmail.com>
2016-10-17 11:01:06 +0100

    Add missing curly brackets.

--- Lectures/Chapter 04 -- More Advanced Programming in C++/src/example14.cpp
+++ Lectures/Chapter 04 -- More Advanced Programming in C++/src/example14.cpp
@@ -2,13 +2,15 @@
 #include <vector>
 int main()
 {
-    std::vector<int> v = { 7, 42, 1, 7, -13 };  // initializer list since C++11
-    v.push_back(11);                            // add another element to the end
-    std::cout << v[5] << std::endl << std::endl;// some containers behave like arrays
-    for (std::vector<int>::iterator ite = v.begin(); ite != v.end(); ++ite)
+    std::vector<int> v = { 7, 42, 1, 7, -13 };   // initializer list since C++11
+    v.push_back(11);                             // add another element to the end
+    std::cout << v[5] << std::endl << std::endl; // some containers behave like arrays
     
+    for (std::vector<int>::iterator ite = v.begin(); ite != v.end(); ++ite)
+    {
         *ite = 5;
         std::cout << *ite << std::endl;         // iterators behave like pointers
-
+    }
+    
     return 0;
 }
